4ef1432318b95d1e87ad7443abbff1315df6b552
Vladislav Usmanov <hangookhelly@gmail.com>
2025-12-04 02:27:03 +0300

    chore: add fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,13 +5,23 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  fonts: {
+    provider: 'google',
+    families: [
+      {
+        name: 'Roboto',
+      }
+    ]
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/icon',
     '@nuxt/test-utils',
     '@primevue/nuxt-module',
-    '@vueuse/nuxt'
+    '@vueuse/nuxt',
+    '@nuxt/fonts'
   ],
 
   primevue: {
