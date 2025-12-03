5338382630b52145790d73ec6d6dedad38cc5b24
Vladislav Usmanov <hangookhelly@gmail.com>
2025-12-03 23:39:56 +0300

    chore: add primevue aura theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import Aura from '@primeuix/themes/aura'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -9,5 +11,13 @@ export default defineNuxtConfig({
     '@nuxt/icon',
     '@nuxt/test-utils',
     '@primevue/nuxt-module'
-  ]
+  ],
+
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
+  }
 })
